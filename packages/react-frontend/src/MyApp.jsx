1f7f64136de45fc6f9e10ac42b5db7b8b92e4189
Amogh Arora <amogharora06@gmail.com>
2026-08-28 22:55:11 -0700

    IE1 2nd commit added form

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,26 +1,11 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      name: "Dee",
-      job: "Aspiring actress",
-    },
-    {
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ]);
+  
+    const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -29,13 +14,13 @@ function MyApp() {
     setCharacters(updated);
   }
 
+
   return (
-    <div className="container">
-      <Table
-        characterData={characters} removeCharacter={removeOneCharacter}
-      />
-    </div>
-  );
+  <div className="container">
+    <Table characterData={characters} removeCharacter={removeOneCharacter} />
+    <Form />
+  </div>
+);
 }
 
 export default MyApp;
